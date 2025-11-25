3e932c13567d864811f11898d22c5f98254844fe
Jeethan1911 <Jeethan.Saldanha@my-aolcc.com>
2025-11-25 16:52:50 -0500

    Assignment

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+alert("Hello! This alert message is coming from Basic_JavaScript_1.js");
+
+function My_First_Function() {
+    var string = "Kiss me, I'm Irish!";
+    var result = string.fontcolor("green");
+    document.getElementById("Irish").innerHTML = result;
+}
+
